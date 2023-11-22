c6bf76e4e2def6fc99a07b2d8a9d0c24ae3f6aab
wei <yuki787817@dayeasy-ai.com>
2023-11-22 17:10:58 +0800

    新增特效

--- components/device/deviceWater-2.vue
+++ components/device/deviceWater-2.vue
@@ -1,20 +1,23 @@
 <template>
   <div>
-    <div>
-      <div class="flex bg-white rounded-lg shadow-md">
-        <div class="flex justify-center">
-          <img src="@/assets/button/sidepage-3.svg" alt="" />
-        </div>
-        <div>
-          <div class="small-title"><p>水箱蓋</p></div>
-          <div class="flex justify-between w-full">
-            <div
-              class="p-4 text-[#717171] text-base font-bold tracking-[0.2rem]"
-            >
-              異常警報
-            </div>
-            <div class="p-4">
-              <DeviceNormal :is-normal="false" />
+    <div class="relative">
+      <div class="absolute top-0 left-0 w-full h-full shadow-blur"></div>
+      <div class="relative z-10">
+        <div class="flex bg-white rounded-lg shadow-md">
+          <div class="flex justify-center">
+            <img src="@/assets/button/sidepage-3.svg" alt="" />
+          </div>
+          <div>
+            <div class="small-title"><p>水箱蓋</p></div>
+            <div class="flex justify-between w-full">
+              <div
+                class="p-4 text-[#717171] text-base font-bold tracking-[0.2rem]"
+              >
+                異常警報
+              </div>
+              <div class="p-4">
+                <DeviceNormal :is-normal="false" />
+              </div>
             </div>
           </div>
         </div>
@@ -23,4 +26,9 @@
   </div>
 </template>
 <script lang="ts" setup></script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.shadow-blur {
+  background: #fffaea;
+  filter: blur(7.5px);
+}
+</style>
